412a7774b403064c2182998211987576c576708a
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-03-29 00:16:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,32 +14,23 @@ end
 end
 
 def remove_element_from_end_of_array(array)
-   array = ["I", "am", "really", "learning", "arrays!"]
-   last_item = array.pop
+
 end
 
-def remove_element_from_start_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  wow = array.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  index_number = 2
-  array.delete_at(index_number)
+
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[-1]
+
 end
 
-def update_element_from_index(array, index_number, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[4] = "totally"
+
 end
