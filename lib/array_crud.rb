79c662313aa72f000c303c0c75105e4ac93008bd
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-03-29 00:16:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,16 @@
 def create_an_empty_array
-create_an_empty_array = []
+
 end
 
 def create_an_array
-create_an_array = [1, 2, 3, 4]  
+
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"] 
-  element = "arrays!"
-  array << element
+
 end
 
-def add_element_to_start_of_array(array, element)
-  array = ["I", "am", "really", "learning"]
-  element = "wow"
-  array.unshift(element)
+
 end
 
 def remove_element_from_end_of_array(array)
